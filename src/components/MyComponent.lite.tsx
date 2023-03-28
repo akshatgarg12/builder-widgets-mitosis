09b71094fbba4140992c10b5774553ac9e4b064c
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-28 18:46:12 +0530

    updated code

--- src/components/MyComponent.lite.tsx
+++ src/components/MyComponent.lite.tsx
@@ -2,15 +2,19 @@ import { useStore, Show, For } from "@builder.io/mitosis";
 
 export default function MyComponent(props) {
   const state = useStore({
-    numbers : [1,2,3,4,5],
-    vowels : ['a', 'e', 'i', 'o', 'u'],
+    numbers : null,
+    vowels : ['a', 'e', 'i', 'o', 'u']
   })
-  if(state.numbers){
-    console.log(state.numbers)
-  }
+
   return (
     <div>
-      <Show when={state.numbers && state.numbers.length}>
+      <Show when={state.numbers}
+        else = {
+            <For each = {state.vowels}>
+              {(vowel) => <h1>vowel : {vowel}</h1>}
+            </For>
+        }
+      >
         <For each={state.numbers}>
           {(number) => <h1>number : {number}</h1>}
         </For>
